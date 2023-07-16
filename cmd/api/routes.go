56859fe6ed8c750be33db475d549fe6e9f7b0b3f
Madhav_Walia <madhavwalia8139@gmail.com>
2023-07-16 23:12:38 +0530

    Added the handler funcs for the create movie and show movie handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,12 +6,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+
+// routes method which returns a httprouter.Router instance containing the application routes
 func(app *application) routes() *httprouter.Router {
 	// Initialize the new httprouter router instance
 	router := httprouter.New()
 
+	// Set the NotFound and MethodNotAllowed error handlers for the router instance
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for our endpoints
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	// Return the httprouter instance
 	return router
