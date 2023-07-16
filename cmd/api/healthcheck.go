502d6daf4a8037029eaa68e2eed2ce7f677ceb37
Madhav_Walia <madhavwalia8139@gmail.com>
2023-07-16 23:11:12 +0530

    Minor Refactor

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // healthcheckHandler for the status and information about the application 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
+	}
+
+	err := app.writeJson(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
