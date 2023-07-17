1978dba4514a917305594985a6e2cd2d232b3161
Madhav_Walia <madhavwalia8139@gmail.com>
2023-07-17 23:25:12 +0530

    Added constraints to the db connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,6 +21,9 @@ type config struct {
 	env string
 	db struct {
 		dsn string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime string
 	}
 }
 
@@ -44,6 +47,11 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", dsn, "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	// Initialize a new logger which writes messages to the standard outstream
@@ -88,6 +96,19 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Set the maximum number of open (in-use + idle) connections in the pool.
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	
+	// Parse the dbMaxIdleTime setting from the config struct
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum idle timeout.
+	db.SetConnMaxIdleTime(duration)
 
 	// Set a 5 second deadline context for a deadline on connection attempts
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
