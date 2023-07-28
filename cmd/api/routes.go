106fb8a699a47719a301f090f3fab1e62bca9236
Madhav_Walia <madhavwalia8139@gmail.com>
2023-07-28 15:29:14 +0530

    Added the registeruserhandler to the list of routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,7 @@ func(app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	// Return the httprouter instance
 	return app.recoverPanic(app.rateLimit(router))
