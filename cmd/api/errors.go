59d9ee4c58492132a58dc440cf1262cb5ec863c4
Madhav_Walia <madhavwalia8139@gmail.com>
2023-07-29 01:23:12 +0530

    Added the invalid authentication response token

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -76,3 +76,12 @@ func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *htt
 	message := "Invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	// Setting the WWW-Authenticate header to tell the client that it needs to send credentials
+	w.Header().Set("WWW-Authenticate", "Bearer")
+	
+	message := "Invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
