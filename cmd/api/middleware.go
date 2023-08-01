c669fc887b550a4662bedaa45b2565815c2828de
Madhav_Walia <madhavwalia8139@gmail.com>
2023-08-01 18:44:49 +0530

    Added the realips wrapper  to help access the original ip of the request coming through the reverse proxy

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -12,6 +11,7 @@ import (
 	"time"
 
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"moviego.madhav.net/internal/data"
 	"moviego.madhav.net/internal/validator"
@@ -74,12 +74,8 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//Check if rate limiting is enabled
 		if !app.config.limiter.enabled {
-			// Extracting the client's IP address from the request
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			// Using the realip package to retrieve the client's real IP address
+			ip := realip.FromRequest(r)
 
 			// Locking the mutex to prevent this code from being executed concurrently
 			mu.Lock()
